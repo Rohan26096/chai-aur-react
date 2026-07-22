c5ea34c1e07b6d016b01ab18d76254f9af0064e5
Rohan <rohan.muni.24@gmail.com>
2026-07-22 13:28:57 +0530

    Intro to Hooks

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,4 +1,3 @@
-// hooks  
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
@@ -6,41 +5,27 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-
-  let [counter,chaiCounter] = useState(15);
-  //let counter = 25;
-  
+  // let counter = 5;
+  let [counter , setCounter] = useState(15); // hooks
   const addvalue = () => {
-    //console.log("Clicked add ",counter )
-    if (counter <= 20) {
-      counter = counter + 1;
-    } else {
-      console.log("Not valid above 20");
-
+    if(counter<20){
+      setCounter(counter+1)
     }
-    chaiCounter(counter)
   }
-
-  const removevalue = () => {
-    //console.log("Clicked remove",counter )
-    if (counter >= 0) {
-      counter = counter - 1;
-    } else {
-      console.log("Not valid below 0");
+  const removevalue= () => {
+    if(counter>0){
+      setCounter(counter-1)
     }
-    chaiCounter(counter)
   }
   return (
     <>
-      <h1> Chai aur React</h1>
-      <h2> Counter value: {counter}</h2>
-
-      <button onClick={addvalue}
-      > Add Value {counter}</button>
-      <br />
-      <button onClick={removevalue}> Remove value {counter}</button>
+      <h1>Chai</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addvalue}>Add Value</button>
+      <br/>
+      <button onClick={removevalue}>Remove value </button>
     </>
   )
 }
 
-export default App
+export default App 
